7f346a0901e49440f06f20c0b37f5ab2964398ca
HyangGyang <60155136+HyangGyang@users.noreply.github.com>
2021-09-24 21:48:03 +0900

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,11 @@
+print("Hello World")
+print("이게 뭐람")
+
+
+'''C:/Users/skssk/AppData/Local/Programs/Python/Python39/python.exe'''
+'''실행파일 장소'''
+'''d:/practice/LanguagePractice/python/+내가 실행할 파일'''
+'''내가 실행할 파일'''
+
+'''최종문법'''
+'''$ C:/Users/skssk/AppData/Local/Programs/Python/Python39/python.exe d:/practice/LanguagePractice/python/HelloWorld.py'''
